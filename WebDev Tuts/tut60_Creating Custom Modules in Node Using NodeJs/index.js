833bb1dc053a4c7f1a8d6bdb89655b185e0af35e
utkarsh-009 <utkarshpatil321@gmail.com>
2023-02-14 01:40:54 +0530

    Creating Custom Modules in Node Using NodeJs

---
+++ WebDev Tuts/tut60_Creating Custom Modules in Node Using NodeJs/index.js
@@ -0,0 +1,8 @@
+// console.log(average([3,4]))
+// const average = require("./mod");
+console.log("This is index.js");
+
+const mod = require("./mod");
+console.log(mod.name) 
+console.log(mod.avg([3,4]))
+
