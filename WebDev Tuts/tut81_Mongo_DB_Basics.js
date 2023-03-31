bce685bb8d282100f3a5de01a9b250d908270853
utkarsh-009 <utkarshpatil321@gmail.com>
2023-03-31 17:19:43 +0530

    Mongo DB basic commands

---
+++ WebDev Tuts/tut81_Mongo_DB_Basics.js
@@ -0,0 +1,17 @@
+// MONGO DB TUTORIAL
+
+// To use database
+use ekart
+
+// To insert a document in collection 'items'
+db.items.insertOne({name: "Asus TUF", price: 86000, rating: 4.4, qty: 100, sold: 90})
+
+// To insert multiple document in collection 'items'
+db.items.insertMany([
+    {name: "Ps5", price: 54000, rating: 4.8, qty: 50, sold: 50},
+    {name: "Xbox Series S", price: 30000, rating: 4.2, qty: 100, sold: 60}
+])
+
+// To find/show all documents in collection 'items'
+db.find.items()
+
