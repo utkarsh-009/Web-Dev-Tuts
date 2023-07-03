51390204cc341de633cd9e8c09134d21bfc3fb1a
utkarsh-009 <utkarshpatil321@gmail.com>
2023-07-04 00:14:53 +0530

    Routers Added

--- React JS/text-utils/src/App.js
+++ React JS/text-utils/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
 
@@ -26,22 +27,42 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
       showAlert("Dark Mode Enabled", "Success");
+      document.title = 'Text Utils - Dark Mode';
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light Mode Enabled", "Success");
+      document.title = 'Text Utils - Light Mode';
     }
   }
 
   return (
     <>
-      <Navbar title="Text Utils" mode={mode} toggleMode={toggleMode} />
+      <Router>
+        <Navbar title="Text Utils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-4" mode={mode}>
+          {/* Routing */}
+          <Routes>
+            {/* exact path: Good practice as if not used it might partial match the path instead of exact
+            , element: Element to which we want to route */}
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter text to analyze" mode={mode} />}></Route>
+          </Routes>
+        </div>
+      </Router>
+
+      {/* <Navbar title="Text Utils" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className="container">
-        <TextForm showAlert={showAlert} heading="Enter Text To Analyse" mode={mode} />
-        {/* <About /> */}
-      </div>
+        <Routes>
+          <Route path="/about" element={<About />}>
+          </Route>
+          <Route path="/" element={<TextForm heading="Enter text to analyze" mode={mode} showAlert={showAlert} />}>
+          </Route>
+        </Routes>
+      </div> */}
     </>
   );
 }
--- React JS/text-utils/src/components/Navbar.js
+++ React JS/text-utils/src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { Link } from 'react-router-dom'
+// NOTE: for anchor tag "a" => use "Link" and for "href" => use "to"
 
 export default function Navbar(props) {
     return (
         <>
             <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
                 <div className="container-fluid">
+                    {/* NOTE: When clicking the "TextUtils" in top left corner, the page relods as we did'nt use react router in it  */}
                     <a className="navbar-brand" href="/">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -14,10 +16,10 @@ export default function Navbar(props) {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">{props.aboutText}</a>
+                                <Link className="nav-link" to="/about">{props.aboutText}</Link>
                             </li>
                         </ul>
                         {/* <form className="d-flex" role="search">
